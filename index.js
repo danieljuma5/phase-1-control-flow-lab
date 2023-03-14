e8d8ae77f1530bc3cabaf4cfee69684475d87d14
danieljuma5 <jumadaniel275@gmail.com>
2023-03-14 10:27:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+
+  let x = 400;
+  let y = 2000;
+  let z = 2500;
+//Writing a scuber ride function using if statements
+if (num < x) {
+  return 'This one is on me!';
+} else if (num >=y && num <= z){
+  return 'I will gladly take your thirty bucks.'
+
+}else if (num > z) {
+  return 'No can do.';
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ return (city == "NYC") ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+   case "generous":
+    return 'Thank you so much.';
+    break;
+    case "not as generous":
+    return 'Thank you.';
+    break;
+    default:
+      return 'Bye.';
+}
 }
